41915199075f3a97935df7eeb769b7e258383706
Dave Okit <fernandookitakula@gmail.com>
2025-09-29 05:51:24 +0200

    New uptade

--- api.js
+++ api.js
@@ -4,12 +4,17 @@ export async function requestApi(query) {
     try {
         const res = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=${key}`)
         if (!res.ok) {
-            throw new Error("Movie not found" + res.status);
+            throw new Error("Movie not found" + res.status);}
+       
+        const data = await res.json();
+        if (data.Response === "False") {
+            throw new Error(data.Error);
         }
-        return res.json()
+        return data;
 
     } catch (error) {
         console.log('Something is wrong', error);
+        return { Error: error.message };
         
     }
 
